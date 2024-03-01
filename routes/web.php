159d85f001ad36966be0af6ff6882b21b7dbb479
nidaamelia <nidaamelia038@gmail.com>
2024-03-01 12:51:06 +0700

    generate report

--- routes/web.php
+++ routes/web.php
@@ -17,9 +17,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/',[BukuController::class,'welcome']);
 
-
-
-
 Auth::routes();
 Route::middleware('auth')->group(function () {
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
